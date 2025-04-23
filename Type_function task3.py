5df7231e8011e2d7a7ed85b23e4270921643c382
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 <diklaeld@gmail.com>
2025-04-23 22:35:17 +0300

    Adding type_function task3

---
+++ Type_function task3.py
@@ -0,0 +1,8 @@
+num = 10
+boolean = True
+string = "Hello"
+print("The type of this function is: ", type(5), "the number is :", num)
+print("The type of this function is: ", type(num), "the bolean is :", boolean)
+print("The type of this function is: ", type(string), "the word is :", string)
+
+
